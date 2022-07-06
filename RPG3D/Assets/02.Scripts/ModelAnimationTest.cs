76c0756c2a1f4be30acc850d9907984bf717a43f
jang <98201675+bbangnaya@users.noreply.github.com>
2022-07-06 22:54:32 +0900

    RPG3D

---
+++ RPG3D/Assets/02.Scripts/ModelAnimationTest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelAnimation : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+            DoRandomAnimation();
+    }
+
+    public void DoRandomAnimation() {
+
+        var ac = animator.runtimeAnimatorController;
+
+        int random = Random.Range(0, ac.animationClips.Length);
+
+        animator.Play(ac.animationClips[random].name);
+
+
+    }
+
+}
